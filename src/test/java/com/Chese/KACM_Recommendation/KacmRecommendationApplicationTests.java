dac05103767cb35a327847c4b98e5bbf53fc10ec
chikien07012006 <kienduong160@gmail.com>
2025-05-11 10:47:25 +0700

    ok

---
+++ src/test/java/com/Chese/KACM_Recommendation/KacmRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chese.KACM_Recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KacmRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
