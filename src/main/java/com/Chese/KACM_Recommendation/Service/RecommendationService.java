8d3f21139d9b3d5ab3bc55adbaafef1746127801
chikien07012006 <kienduong160@gmail.com>
2025-06-14 19:31:23 +0700

    heap and hashmamp

--- src/main/java/com/Chese/KACM_Recommendation/Service/RecommendationService.java
+++ src/main/java/com/Chese/KACM_Recommendation/Service/RecommendationService.java
@@ -7,28 +7,151 @@ import com.Chese.KACM_Recommendation.model.FoodDetail;
 import com.Chese.KACM_Recommendation.entities.Restriction;
 import com.Chese.KACM_Recommendation.model.FoodSummary;
 
+
+class maxheap{
+    FoodSummary[] heap;
+    int currentsize;
+    int capacity;
+    HashMap<String, Integer> position;
+
+    maxheap(int capa){
+        this.heap=new FoodSummary[capa];
+        this.currentsize=0;
+        this.capacity=capa;
+        this.position = new HashMap<>();
+    }
+
+    int parent(int i) {return (i-1)/2;}
+    int left(int i) {return 2*i+1;}
+    int right(int i) {return 2*i+2;}
+
+    void insert(FoodSummary food){
+        if(currentsize==capacity) return;
+        heap[currentsize]=food;
+        position.put(food.getId(), currentsize);
+        int i=currentsize;
+        currentsize=currentsize+1;
+
+        while(i!=0&&heap[i].getScore()>heap[parent(i)].getScore()){
+            FoodSummary temp = heap[parent(i)];
+            heap[parent(i)] = heap[i];
+            heap[i] = temp;
+
+            position.put(heap[i].getId(), i);
+            position.put(heap[parent(i)].getId(), parent(i));
+
+            i = parent(i);
+        }
+    }
+
+    void maxHeapify(int i){
+        int l=left(i);
+        int r=right(i);
+        int largest=i;
+
+        if(l<currentsize && heap[l].getScore()>heap[largest].getScore()) largest=l;
+        if(r<currentsize && heap[r].getScore()>heap[largest].getScore()) largest=r;
+
+        if(largest!=i){
+            FoodSummary temp=heap[largest];
+            heap[largest]=heap[i];
+            heap[i]=temp;
+
+            position.put(heap[i].getId(), i);
+            position.put(heap[largest].getId(), largest);
+
+            maxHeapify(largest);
+        }
+    }
+
+    void delete(int i){
+        if(currentsize==1){
+            currentsize=currentsize-1;
+            return;
+        }
+
+        heap[i]=heap[--currentsize];
+        position.put(heap[i].getId(), currentsize);
+
+        while(i!=0&&heap[i].getScore()>heap[parent(i)].getScore()){
+            FoodSummary temp = heap[parent(i)];
+            heap[parent(i)] = heap[i];
+            heap[i] = temp;
+
+            position.put(heap[i].getId(), i);
+            position.put(heap[parent(i)].getId(), parent(i));
+
+            i = parent(i);
+        }
+
+        maxHeapify(i);
+    }
+
+    ArrayList<FoodSummary> sort(){
+        FoodSummary[] temp = heap.clone();
+        int new_size = currentsize;
+        ArrayList<FoodSummary> newSortedList = new ArrayList<>();
+        
+        while(currentsize>0){
+            newSortedList.add(extractMax_sort(temp));
+        }
+        currentsize=new_size;
+        return newSortedList;
+    }
+
+    FoodSummary extractMax_sort(FoodSummary[] arr){
+        if(currentsize<=0) return null;
+        if(currentsize==1) return arr[--currentsize];
+        FoodSummary root=arr[0];
+        arr[0]=arr[--currentsize];
+        maxHeapify_sort(arr, 0);
+        return root;
+    }
+
+    void maxHeapify_sort(FoodSummary[] arr, int i){
+        int l=left(i);
+        int r=right(i);
+        int largest=i;
+
+        if(l<currentsize && arr[l].getScore()>arr[largest].getScore()) largest=l;
+        if(r<currentsize && arr[r].getScore()>arr[largest].getScore()) largest=r;
+
+        if(largest!=i){
+            FoodSummary temp=arr[largest];
+            arr[largest]=arr[i];
+            arr[i]=temp;
+            maxHeapify_sort(arr, largest);
+        }
+    }
+
+
+}
+
+
 @Service
 public class RecommendationService {
 
     private final FoodService svc;
+    List<FoodDetail> foods;
+    maxheap diet_graph;
+    maxheap cusine_graph;
 
     public RecommendationService(FoodService svc) {
         this.svc = svc;
+        this.foods = this.svc.getDetail();
+        this.diet_graph = new maxheap(this.foods.size());
+        this.cusine_graph = new maxheap(this.foods.size());
     }
 
     public List<FoodSummary> getRecommendationsByDiet(Restriction restriction) {
-        List<FoodDetail> foods = svc.getDetail();
+
+        if(diet_graph.currentsize>0){
+            return diet_graph.sort();
+        }
+
         List<String> hateTastes = restriction.gethate_taste_adjectives();
         List<String> foodAllergys = restriction.getfood_allergy();
         List<String> diet = restriction.getdiet();
-        List<String> preferredCuisines = restriction.getPreferred_Cuisines();
-
-        PriorityQueue<FoodSummary> sortByDiet = new PriorityQueue<>((a, b) -> {
-            if(a.getScore() > b.getScore()) return -1;
-            if(a.getScore() < b.getScore()) return 1;
-            return 0;
-
-        }); 
 
         for (FoodDetail food : foods) {
             food.setScore(0); 
@@ -57,34 +180,23 @@ public class RecommendationService {
 
             for(String tag : food.getTags()){
                 if(diet.contains(tag)){
-                    food.setScore(food.getScore()+5);
-                }
-            }
-
-            for(String tag : food.getTags()){
-                if(preferredCuisines.contains(tag)){
                     food.setScore(food.getScore()+1);
                 }
             }
-            sortByDiet.add(food);
-            System.out.println(food.getScore());
+            diet_graph.insert(food);
         }
-        return new ArrayList<>(sortByDiet);
+        return diet_graph.sort();
     }
 
     public List<FoodSummary> getRecommendationsByCusine(Restriction restriction) {
-        List<FoodDetail> foods = svc.getDetail();
+
+        if(cusine_graph.currentsize>0){
+            return cusine_graph.sort();
+        }
+
         List<String> hateTastes = restriction.gethate_taste_adjectives();
         List<String> foodAllergys = restriction.getfood_allergy();
-        List<String> diet = restriction.getdiet();
         List<String> preferredCuisines = restriction.getPreferred_Cuisines();
-
-        PriorityQueue<FoodSummary> sortByCusine = new PriorityQueue<>((a, b) -> {
-            if(a.getScore() > b.getScore()) return -1;
-            if(a.getScore() < b.getScore()) return 1;
-            return 0;
-
-        }); 
 
         for (FoodDetail food : foods) {
             food.setScore(0); 
@@ -106,23 +218,21 @@ public class RecommendationService {
                     break;
                 }
             }
-            if(flag == true) continue;
-
-            for(String tag : food.getTags()){
-                if(diet.contains(tag)){
-                    food.setScore(food.getScore()+1);
-                }
-            }
 
             for(String tag : food.getTags()){
                 if(preferredCuisines.contains(tag)){
-                    food.setScore(food.getScore()+5);
+                    food.setScore(food.getScore()+1);
                 }
             }
-            sortByCusine.add(food);
-            System.out.println(food.getScore());
+            cusine_graph.insert(food);
         }
 
-        return new ArrayList<>(sortByCusine);
+        return cusine_graph.sort();
     }
+
+    public void delete(String id){
+        diet_graph.delete(diet_graph.position.get(id));
+        cusine_graph.delete(cusine_graph.position.get(id));
+    }
+
 }
