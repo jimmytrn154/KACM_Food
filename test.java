ea3b2129c4eb58e534efe9d0785bd588014913cd
Jimmy Tran <chunnam1519@gmail.com>
2025-06-15 09:15:24 +0700

    remove redundant files

--- test.java
+++
@@ -1,8 +0,0 @@
-//Program to capitalize first letters of words
-public class test {
-
-	public static void main(String[] args) {
-		String input = "java is cool";
-		System.out.println("Final message: ");
-	}
-}
